15b019c5ead58894b418dac61d23441c623bcf2f
Nanji Lakan <86968487+Shaelle11@users.noreply.github.com>
2023-11-23 14:07:58 +0100

    Navbar done and responsive, clicking the bell shows notifications and clcking the users name has a profile drpdown

--- app.js
+++ app.js
@@ -1,16 +1,68 @@
-function myDropdown() {
-    document.getElementById("notification-message").classList.toggle("show");
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  const notificationBell= document.getElementById('notification-bell');
+  const notificationMessage = document.getElementById('notification-message');
+
   
-  window.onclick = function(event) {
-    if (!event.target.matches('.bell')) {
-      var dropdowns = document.getElementsByClassName("notification_message");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (!openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+    // Toggle the dropdown on button 
+    notificationBell.addEventListener('click', function () {
+      const isOpen = notificationMessage.style.display === 'block';
+  
+      notificationMessage.style.display = isOpen ? 'none' : 'block';
+      notificationBell.setAttribute('aria-expanded', !isOpen);
+      
+      // Set focus on the dropdown 
+      if (!isOpen) {
+        notificationMessage.focus();
       }
-    }
+    });
+  
+    // Hide the dropdown on Escape key press
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        notificationMessage.style.display = 'none';
+        notificationBell.setAttribute('aria-expanded', 'false');
+      }
+    });
+  
+    document.addEventListener('click', function (event) {
+      if (!notificationBell.contains(event.target) && !notificationMessage.contains(event.target)) {
+        notificationMessage.style.display = 'none';
+        notificationBell.setAttribute('aria-expanded', 'false');
+      }
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function(){
+    const userName = document.getElementById('user-name');
+    const profileDrop = document.getElementById('profile');
+
+    userName.addEventListener('click', function() {
+      const isOpen = profileDrop.style.display === 'block';
+
+      profileDrop.style.display = isOpen ? 'none' : 'block';
+      userName.setAttribute('aria-expanded', !isOpen);
+
+      if(!isOpen){
+        profileDrop.focus();
+      }
+    })
+
+    document.addEventListener('keydown', function(event){
+      if(event.key === 'Escape'){
+        profileDrop.style.display = 'none';
+        userName.setAttribute('aria-expanded', 'false');
+      }
+    })
+
+    document.addEventListener('click', function (event){
+      if(!userName.contains(event.target)&& !profileDrop.contains(event.target)){
+        profileDrop.style.display = 'none';
+        userName.setAttribute('aria-expanded', 'true');
+      }
+    });
+  });
+
+  function closeAlert(){
+    const subscrption = document.getElementById('alert-subscription')
+    subscrption.style.display = 'none';
   }
